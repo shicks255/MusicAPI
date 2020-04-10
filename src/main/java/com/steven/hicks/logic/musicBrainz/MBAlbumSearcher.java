dde1729b9ecacf5cb42f4da6a3a9222f65d60b0e
shicks <shicks255@yahoo.com>
2020-04-09 22:19:03 -0500

    MB for album searching

--- src/main/java/com/steven/hicks/logic/musicBrainz/MBAlbumSearcher.java
+++ src/main/java/com/steven/hicks/logic/musicBrainz/MBAlbumSearcher.java
@@ -1,24 +1,28 @@
 package com.steven.hicks.logic.musicBrainz;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Iterator;
 
 public class MBAlbumSearcher {
+    private static ObjectMapper objectMapper = new ObjectMapper();
     private static String MUSIC_BRAINZ_ENDPOINT = "https://musicbrainz.org/ws/2/";
-    private static String ALBUM_ART_ENDPOINT = "https://musicbrainz.org/ws/2/";
+    private static String ALBUM_ART_ENDPOINT = "http://coverartarchive.org/release/";
 
-    public void searchForAlbum(String albumTitle) {
-        StringBuilder endpoint = new StringBuilder(MUSIC_BRAINZ_ENDPOINT + "release-group?query=artist:");
-        endpoint.append(artistName);
-        endpoint.append("&fmt=json");
+    public void searchForAlbumsByArtist(String artistMbid) {
+        StringBuilder endPoint = new StringBuilder(MUSIC_BRAINZ_ENDPOINT + "release-group?query=arid:");
+        endPoint.append(artistMbid);
+        endPoint.append("%20AND%20primarytype:album%20AND%20status:official&fmt=json");
 
-        try
-        {
-            URL url = new URL(endpoint.toString());
+        try {
+            URL url = new URL(endPoint.toString());
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
 
             connection.setRequestProperty("accept", "application/json");
@@ -32,17 +36,102 @@ public class MBAlbumSearcher {
                     data.append(input);
             }
 
-            JsonNode node = m_objectMapper.readTree(data.toString());
-            JsonNode artists = node.get("artists");
+            JsonNode node = objectMapper.readTree(data.toString());
+            JsonNode albums = node.get("release-groups");
 
-            System.out.println(artists);
+            //iterate through each release group
+            for (Iterator<JsonNode> it = albums.iterator(); it.hasNext(); )
+            {
+                JsonNode releaseGroup = it.next();
+                //iterate through releases until we get album info from albumArtArchive
+                JsonNode releases = releaseGroup.get("releases");
 
-            return artists;
+                //iterate through each release until we find images
+                for (Iterator<JsonNode> itt = releases.iterator(); itt.hasNext(); ) {
+                    JsonNode release = itt.next();
+                    String id = release.get("id").asText();
+                    JsonNode images = getAlbumImage(id);
+                    if (images != null) {
+                        ((ObjectNode)releaseGroup).putArray("images").add(images);
+                        break;
+                    }
+                }
+            }
+
+            System.out.println(albums);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private JsonNode getAlbumImage(String releaseId) {
+        String endPoint = ALBUM_ART_ENDPOINT + releaseId;
+
+        try
+        {
+            URL url = new URL(endPoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestProperty("accept", "application/json");
+            connection.setRequestMethod("GET");
+
+            StringBuilder data = new StringBuilder();
+            String input;
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));)
+            {
+                while ((input = in.readLine()) != null)
+                    data.append(input);
+            }
+
+            if (data.length() == 0)
+                return null;
+
+            JsonNode node = objectMapper.readTree(data.toString());
+            JsonNode images = node.get("images");
+            return images;
+
         } catch (Exception e)
         {
-            e.printStackTrace();
+            System.out.println(e);
         }
 
         return null;
     }
+
+//    public void searchForAlbum(String albumTitle) {
+////        https://musicbrainz.org/ws/2/release-group?query=arid:bdacc37b-8633-4bf8-9dd5-4662ee651aec
+////        %20AND%20primarytype:album%20AND%20status:official&fmt=json
+//        StringBuilder endpoint = new StringBuilder(MUSIC_BRAINZ_ENDPOINT + "release-group?query=arid:");
+//        endpoint.append(artistName);
+//        endpoint.append("&fmt=json");
+//
+//        try
+//        {
+//            URL url = new URL(endpoint.toString());
+//            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+//
+//            connection.setRequestProperty("accept", "application/json");
+//            connection.setRequestMethod("GET");
+//
+//            StringBuilder data = new StringBuilder();
+//            String input;
+//            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8")))
+//            {
+//                while ((input = in.readLine()) != null)
+//                    data.append(input);
+//            }
+//
+//            JsonNode node = m_objectMapper.readTree(data.toString());
+//            JsonNode artists = node.get("artists");
+//
+//            System.out.println(artists);
+//
+//            return artists;
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
 }
